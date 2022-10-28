65462334c169acdf6d3311a981feead4e969a759
harshkul <kulshreshtha.harsh5@gmail.com>
2022-10-28 18:17:55 +0530

    add employee rest api
    add employee rest api

--- api/EmpMasterGetEmployes/__init__.py
+++ api/EmpMasterGetEmployes/__init__.py
@@ -5,20 +5,18 @@ import azure.functions as func
 
 def main(req: func.HttpRequest)->func.HttpResponse:
     logging.info('Triggered Get Employee python function.')
-
-    id =int(req.route_params.get("id")) 
-
+    dbconString=""
     cnxn_str = ("Driver={SQL Server Native Client 11.0};"
             "Server=rewardsdbserver.database.windows.net;"
             "Database=rewardsdbindia;"
             "UID=rwadmin;"
             "PWD=Monika150687;")
-    
     cnxn = pyodbc.connect(cnxn_str)
     # build up our query string
-    query = ("SELECT * FROM EmpMaster WHERE EmpID = ?")
+    query = ("SELECT * FROM EmpMaster")
+
     # execute the query and read to a dataframe in Python
-    data = pd.read_sql(query, cnxn,params=[id])
+    data = pd.read_sql(query, cnxn)
     return func.HttpResponse(
              data.to_json(orient="index",date_format='iso'),
              status_code=200
